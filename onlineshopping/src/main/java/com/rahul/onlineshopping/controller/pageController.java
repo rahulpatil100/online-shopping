5b29ce992af77e03ad07a7510241999345c381db
Rahul Patil <rahulcs629@gmail.com>
2017-09-02 15:30:06 +0530

    Successfully Completed Static Resources

---
+++ onlineshopping/src/main/java/com/rahul/onlineshopping/controller/pageController.java
@@ -0,0 +1,23 @@
+package com.rahul.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome to spring");
+		return mv;
+	}
+
+	
+	
+	
+	
+	
+}
